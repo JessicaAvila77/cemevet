d629f1b2d7c2ab8a9ef27db248a5ffab4ffc404a
Esteph11 <esteportilloluna11@gmail.com>
2025-03-22 07:10:02 -0600

    Home
    Falta

---
+++ src/app/data/tarjeta-servicios.ts
@@ -0,0 +1,57 @@
+import { ITarjetaServicios } from "../types/ITarjetaServicios";
+
+
+export const datos: ITarjetaServicios[] = [
+    {
+        icono: "stethoscope",
+        texto: "Consulta médica"
+    },
+    {
+        icono: "hospital",
+        texto: "Cirujia y hospiltalización"
+    },
+    {
+        icono: "prescription",
+        texto: "Radiografía"
+    },
+    {
+        icono: "shop",
+        texto: "Pet Shop"
+    },
+    {
+        icono: "dog",
+        texto: "Salón de belleza"
+    },
+    {
+        icono: "van-shuttle",
+        texto: "Servicio de transporte"
+    },
+    {
+        icono: "syringe",
+        texto: "Vacunación"
+    },
+    {
+        icono: "certificate",
+        texto: "Emisión de certificados"
+    },
+    {
+        icono: "prescription-bottle",
+        texto: "Farmacia"
+    },
+    {
+        icono: "flask",
+        texto: "Laboratorio"
+    },
+    {
+        icono: "handshake-angle",
+        texto: "Asistencia técnica a fincas ganaderas"
+    },
+    {
+        icono: "person-rays",
+        texto: "Ultrasonido"
+    },
+
+
+
+
+]
--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { TarjetaComponent } from "../../Components/tarjeta/tarjeta.component";
+import { TarjetaServicioComponent } from '../../Components/tarjeta-servicio/tarjeta-servicio.component';
+import { ListadoTarjetaComponent } from '../../Components/listado-tarjeta/listado-tarjeta.component';
+
+import { ITarjetaServicios } from '../../types/ITarjetaServicios';
+import { datos } from '../../data/tarjeta-servicios';
+
 
 @Component({
   selector: 'app-servicios',
-  imports: [TarjetaComponent],
+  imports: [ListadoTarjetaComponent],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.scss'
 })
 export class ServiciosComponent {
+
+  datosTarjetaServicios: ITarjetaServicios[] = datos 
 
 }
