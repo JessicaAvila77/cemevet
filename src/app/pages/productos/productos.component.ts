577e7eb74d4464e388f226c918f2ca2425fe5832
JessicaAvila77 <jessica.avilaunah@gmail.com>
2025-03-27 09:22:47 -0600

    Actualizando pagina productos

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Categoria {
+  nombre: string;
+  icono: string; // Clases de FontAwesome
+}
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.scss'
+  styleUrls: ['./productos.component.scss']
 })
 export class ProductosComponent {
+  categorias: Categoria[] = [
+    { nombre: 'Accesorios', icono: 'fa-solid fa-paw' },
+    { nombre: 'Alimentos', icono: 'fa-solid fa-bowl-food' },
+    { nombre: 'Medicamentos', icono: 'fa-solid fa-capsules' },
+    { nombre: 'Juguetes', icono: 'fa-solid fa-bone' },
+    { nombre: 'Perfumeria', icono: 'fa-solid fa-spray-can' },
+  ];
 
+  whatsappLink(categoria: string): string {
+    return `https://wa.me/50496455137?text=Hola%2C%20quiero%20más%20información%20sobre%20productos%20de%20${encodeURIComponent(categoria)}`;
+  }
 }
+
